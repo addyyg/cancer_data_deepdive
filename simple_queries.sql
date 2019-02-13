37c181d8fe053a4d3ab5292eee2201f49cca6f0d
wilhelmkang <47461577+wilhelmkang@users.noreply.github.com>
2019-02-13 15:50:13 -0600

    Update simple_queries.sql

--- simple_queries.sql
+++ simple_queries.sql
@@ -2,3 +2,8 @@
 select age, sex, site, year from cancer_stats.brain_cancer
 where behavior = "Malignant" and age = "20+"
 order by year;
+
+--compares mortality counts of males in each area/state in 2008
+select area, count, year from cancer_stats.CancerByArea
+where year = "2008" and site = "All Cancer Sites Combined" and race = "All Races" and sex = "Male" and event_type = "Mortality"
+order by area;
