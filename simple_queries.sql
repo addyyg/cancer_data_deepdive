083be5dbec8afc749a2ffe7dddb797aac91de1f4
wilhelmkang <47461577+wilhelmkang@users.noreply.github.com>
2019-02-13 20:42:41 -0600

    Update simple_queries.sql

--- simple_queries.sql
+++ simple_queries.sql
@@ -7,3 +7,8 @@ order by year;
 select area, count, year from cancer_stats.CancerByArea
 where year = "2008" and site = "All Cancer Sites Combined" and race = "All Races" and sex = "Male" and event_type = "Mortality"
 order by area;
+
+--Shows the non lethal cancer types and its demographic ordered by the most common to least common
+select site, sex, race, event_type from cancer_stats.CancerBySite
+where event_type != "Mortality"
+order by count desc;
