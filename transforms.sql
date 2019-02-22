d6a982c0fb5333f8f478ae079262211fc4d0626c
addyyg <47610434+addyyg@users.noreply.github.com>
2019-02-22 12:06:20 -0600

    Update transforms.sql

--- transforms.sql
+++ transforms.sql
@@ -9,3 +9,14 @@ select serialid, age, behavior, count, population, sex, site, year
 from cancer_stats.brain_cancer
 where count is not null and age != "0-19"
 order by serialid
+--cancer by area
+--Cancer by area separated by area focused on incidence
+select serialid, area, count, population, sex, site, year, crude_rate, age_adjusted_rate, race, event_type
+from cancer_stats.CancerByArea
+where count is not null and population is not null and year != "2008-2012" and sex = "Male and Female" and race = "All Races" and site = "All Cancer Sites Combined" and site is not null and crude_rate != 0 and event_type = "Incidence"
+order by serialid
+--Cancer by area separated by area focused on mortality
+select serialid, area, count, population, sex, site, year, crude_rate, age_adjusted_rate, race, event_type
+from cancer_stats.CancerByArea
+where count is not null and population is not null and year != "2008-2012" and sex = "Male and Female" and race = "All Races" and site = "All Cancer Sites Combined" and site is not null and crude_rate != 0 and event_type = "Mortality"
+order by serialid
