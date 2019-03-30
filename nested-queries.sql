2c507bfd7cbc0465c54f473b14454523211839b3
addyyg <47610434+addyyg@users.noreply.github.com>
2019-03-29 23:11:32 -0500

    Update nested-queries.sql

--- nested-queries.sql
+++ nested-queries.sql
@@ -22,7 +22,7 @@ select year, age, avg(age_adjusted_ci_upper), avg(age_adjusted_rate )
 from cancer_stats.AgeRatesOver10
 group by year, age
 having avg(age_adjusted_rate) < (select avg(age_adjusted_rate) from cancer_stats.Cancer_By_Area_Incidence)
---a join to see if there are any eye and orbit instances in alaska based on similar count numbers 
+--a join to see if there are any eye and orbit instances in alaska based on similar count numbers. 
 select c.count, c.population
 from cancer_stats.Cancer_By_Area_Mortality c 
 join 
@@ -33,14 +33,36 @@ join
    ) as a
 on c.count = a.count
 where c.Area = 'Alaska'
---
-select c.count, c.population
-from cancer_stats.Cancer_By_Area_Mortality c 
-join 
+--flipped the previous query and looked for cases of acute lymphocytic cancer in the year 1999 and joined it on the basis of counts recorded. did a left join to get the counts from the site table regardless of a match.
+select s.year, s.count, s.population
+from cancer_stats.CancerBySiteV2 s 
+left join 
   (
     select count 
-    from [cancer_stats.CancerBySiteV2] s
-    where s.site = "Eye and Orbit"
+    from [cancer_stats.Cancer_By_Area_Incidence] c
+    where c.area = "Alabama"
    ) as a
-on c.count = a.count
-where c.Area = 'Alaska'
+on s.count = a.count
+where s.site = 'Acute Lymphocytic' AND s.year = 1999
+--Trying to examine the cases of where the populations for both mortality nad incidence in the state of california were the same and greater than 3.5 million
+select s.year, s.count, s.population
+from cancer_stats.Cancer_By_Area_Mortality s 
+join 
+  (
+    select population
+    from [cancer_stats.Cancer_By_Area_Incidence] c
+    where c.area = "California"
+   ) as a
+on s.population = a.population
+where s.population > 3500000
+--This is where the mortality is compared to the same site of acute lymphocytic of a different year
+select s.year, s.count, s.population
+from cancer_stats.CancerBySiteV2 s 
+left join 
+  (
+    select count 
+    from [cancer_stats.Cancer_By_Area_Mortality] c
+    where c.area = "Michigan"
+   ) as a
+on s.count = a.count
+where s.site = 'Acute Lymphocytic' AND s.year = 2002
