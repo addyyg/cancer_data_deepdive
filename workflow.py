6e20997925823416f86ae1b21b7020841cdb2eb1
addyyg <47610434+addyyg@users.noreply.github.com>
2019-05-03 17:52:14 -0500

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,3 +1,34 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['adhiesp@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'tutorial', default_args=default_args, schedule_interval=timedelta(days=1))
+
+#t1, etc. are tasks
+t1 = BashOperator(
+  task_id='change_date',
+  bash_command='date',
+  dag=dag)
+
+t2 = BashOperator(
+  task_id='sleep'
+  bash_command=
+  retries=
+  dag=dag)
+
+templated_command= """
