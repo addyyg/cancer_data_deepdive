c5e81467bc89f8c9810c00d06f3571bdf3da4bd1
addyyg <47610434+addyyg@users.noreply.github.com>
2019-05-03 23:44:10 -0500

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,34 +1,94 @@
-from airflow import DAG
+import datetime
+from airflow import models
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
-    'email': ['adhiesp@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+from airflow.operators.dummy_operator import DummyOperator
+
+default_dag_args = {
+    'start_date': datetime.datetime(2019, 4, 23)
 }
+###### SQL variables ###### 
+raw_dataset = 'cancer_trials'
+new_dataset = 'workflow'
+sql_cmd_start = 'bq query --use_legacy_sql=false '
 
-dag = DAG(
-    'tutorial', default_args=default_args, schedule_interval=timedelta(days=1))
+sql_arm= 'create table ' + new_dataset + '.Arm_Temp as ' \
+           'from ' + raw_dataset + '.ArmGroup ' \
+           'select distinct nct_number,arm_group_label,arm_group_type,description,serialid ' \
+           'from cancer_trials.NIHClinicalTrials_Arm_Group' \
+           'where arm_group_label like '%cancer%' or description like '%cancer%' '\
+           'order by nct_number'
 
-#t1, etc. are tasks
-t1 = BashOperator(
-  task_id='change_date',
-  bash_command='date',
-  dag=dag)
+sql_location='create table ' + new_dataset + '.Location_Temp as ' \
+           'from ' + raw_dataset + '.Location ' \
+           'select distinct nct_number,facility_name,facility_city,facility_state,facility_country,serialid ' \
+           'from cancer_trials.NIHClinicalTrials_Location ' \
+           'where facility_country == 'United States' '\
+           'and nct_number is not null '\
+           'order by nct_number'
 
-t2 = BashOperator(
-  task_id='sleep'
-  bash_command=
-  retries=
-  dag=dag)
+sql_primary = 'create table ' + new_dataset + '.Primary_Temp as ' \
+           'from ' + raw_dataset + '.ArmGroup ' \
+           'select distinct nct_number,measure,time_frame,description,serialid ' \
+           'from cancer_trials.NIHClinicalTrials_Primary_Outcomes' \
+           'where measure is not null'\
+           'order by nct_number'
 
-templated_command= """
+sql_secondary= 'create table ' + new_dataset + '.Secondary_Temp as ' \
+           'from ' + raw_dataset + '.ArmGroup ' \
+           'select distinct nct_number,measure,time_frame,description,serialid ' \
+           'from cancer_trials.NIHClinicalTrials_Secondary_Outcomes' \
+           'where measure is not null'\
+           'order by nct_number'
+
+###### Beam variables ######          
+LOCAL_MODE=1 # run beam jobs locally
+DIST_MODE=2 # run beam jobs on Dataflow
+
+mode=LOCAL_MODE
+
+if mode == LOCAL_MODE:
+    main_script = 'transform_trials_main_single.py'
+    
+if mode == DIST_MODE:
+    main_script = 'transform_trials_main_single.py'
+
+###### DAG section ###### 
+with models.DAG(
+        'workflow',
+        schedule_interval=datetime.timedelta(days=1),
+        default_args=default_dag_args) as dag:
+    ###### SQL tasks ######
+    delete_dataset = BashOperator(
+            task_id='delete_dataset',
+            bash_command='bq rm -r -f workflow')
+                
+    create_dataset = BashOperator(
+            task_id='create_dataset',
+            bash_command='bq mk workflow')
+    
+    create_arm_table = BashOperator(
+            task_id='create_arm_table',
+            bash_command=sql_cmd_start + '"' + sql_arm + '"')
+    
+    create_primary_table = BashOperator(
+            task_id='create_primary_table',
+            bash_command=sql_cmd_start + '"' + sql_primary + '"')
+    
+    create_secondary_table = BashOperator(
+            task_id='create_arm_table',
+            bash_command=sql_cmd_start + '"' + sql_secondary + '"')  
+    
+    create_location_table = BashOperator(
+            task_id='create_location_table',
+            bash_command=sql_cmd_start + '"' + sql_location + '"')    
+    
+    ###### Beam tasks ######     
+    main_beam = BashOperator(
+            task_id='main_beam',
+            bash_command='python /Users/adhiesp/airflow/dags/' + main_script)
+
+    transition = DummyOperator(task_id='transition')
+            
+    delete_dataset >> create_dataset >> [main_beam]>> transition
+    transition >> [create_arm_table, create_primary_table, create_secondary_table, create_location_table]
+    
